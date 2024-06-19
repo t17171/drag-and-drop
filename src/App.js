21a5844c011436bd827a8526fc1cedd3490013da
t17171 <terui1106x@gmail.com>
2024-06-19 10:55:53 +0900

    見た目完成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 //import { DragDropContext } from "react-beautiful-dnd";
+import React from "react";
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
-function App() {
+const DragDrop = () => {
   return (
     <div className="App">
       <DragDropContext>
@@ -21,11 +22,23 @@ function App() {
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                   >
+                    オプション0
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="option1" index={1}>
+                {(provided) => (
+                  <div
+                    className="option"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
                     オプション1
                   </div>
                 )}
               </Draggable>
-              <Draggable draggableId="option0" index={0}>
+              <Draggable draggableId="option2" index={2}>
                 {(provided) => (
                   <div
                     className="option"
@@ -37,12 +50,24 @@ function App() {
                   </div>
                 )}
               </Draggable>
+              <Draggable draggableId="option3" index={3}>
+                {(provided) => (
+                  <div
+                    className="option"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    オプション3
+                  </div>
+                )}
+              </Draggable>
             </div>
           )}
         </Droppable>
       </DragDropContext>
     </div>
   );
-}
+};
 
-export default App;
+export default DragDrop;
