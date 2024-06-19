df746cfd274a289241d623fadec1450271e691bc
t17171 <terui1106x@gmail.com>
2024-06-19 10:40:43 +0900

    サブモジュールの変更

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+//import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext>
+        <Droppable droppableId="chars">
+          {(provided) => (
+            <div
+              className="optionList"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <Draggable draggableId="option0" index={0}>
+                {(provided) => (
+                  <div
+                    className="option"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    オプション1
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="option0" index={0}>
+                {(provided) => (
+                  <div
+                    className="option"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    オプション2
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
